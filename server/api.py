04ef10d4fef7b65bcde57106fd9121efa5b3b851
nati-m <it.natnael.mekonnen@gmail.com>
2021-05-18 21:38:07 +0300

    added python libraries

--- server/api.py
+++ server/api.py
@@ -1,9 +1,14 @@
 from flask import Flask, session, render_template, request, redirect, url_for, jsonify
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 load_dotenv()
+
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
 
 
 if __name__ == '__main__':
