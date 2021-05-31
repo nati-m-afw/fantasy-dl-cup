6bbc90a7f7a3b8f3646d1d18aa603a392673f28e
nati-m <it.natnael.mekonnen@gmail.com>
2021-05-31 15:57:10 +0300

    updated UI for MyTeam

---
+++ client/src/components/Navigation.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="sticky-nav">
+        <router-link to="/myteam"  :class="activePage == 'MyTeam' ? 'active' : ''">My team</router-link>
+        <router-link to="" :class="this.activePage == 'myTeam' ? 'active' : ''">Points</router-link>
+        <router-link to="" :class="this.activePage == 'myTeam' ? 'active' : ''">Transfers</router-link>
+        <router-link to="" :class="this.activePage == 'myTeam' ? 'active' : ''">League</router-link>
+        <router-link to="" :class="this.activePage == 'myTeam' ? active : ''">Table</router-link>
+        <router-link to="" :class="this.activePage == 'myTeam' ? 'active' : ''">Fixtures</router-link>
+        <router-link to="" :class="this.activePage == 'myTeam' ? 'active' : ''">Stats</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["activePage"],
+    };
+</script>
+
+<style scoped>
+
+</style>
